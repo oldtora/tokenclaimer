e7e64c8af7747f2dc42e3046767270b51b96a8f2
oldtora <50335753+oldtora@users.noreply.github.com>
2023-04-09 11:54:18 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,69 +1,46 @@
 from web3 import Web3
-from dotenv import dotenv_values
+from decimal import Decimal
 
-# Загрузить переменные из файла .env
-config = dotenv_values(".env")
+# Указываем адрес контракта и его ABI
+contract_address = "0x1234567890123456789012345678901234567890"
+contract_abi = [ ... ]
 
-# Установить переменные
-PRIVATE_KEY_1 = config["PRIVATE_KEY_1"]
-ADDRESS_1 = config["ADDRESS_1"]
-PRIVATE_KEY_2 = config["PRIVATE_KEY_2"]
-ADDRESS_2 = config["ADDRESS_2"]
-TOKEN_ADDRESS = config["TOKEN_ADDRESS"]
-TOKEN_ABI = config["TOKEN_ABI"]
-GAS_PRICE = config["GAS_PRICE"]
-GAS_LIMIT = config["GAS_LIMIT"]
+# Указываем адреса кошельков и их соответствующие приватные ключи
+wallet_addresses = ["0x...", "0x...", "0x...", "0x...", "0x..."]
+private_keys = ["11..", "22..", "33..", "44..", "55.."]
 
-# Создать объект Web3
-w3 = Web3(Web3.HTTPProvider(config["WEB3_PROVIDER_URI"]))
+# Устанавливаем провайдер для работы с блокчейном (в данном случае Infura)
+w3 = Web3(Web3.HTTPProvider('https://mainnet.infura.io/v3/your-infura-project-id'))
 
-# Получить экземпляр контракта токена
-token_contract = w3.eth.contract(address=w3.toChecksumAddress(TOKEN_ADDRESS), abi=TOKEN_ABI)
+# Получаем объект контракта
+contract = w3.eth.contract(address=contract_address, abi=contract_abi)
 
-# Функция для получения общего количества токенов на всех кошельках
-def get_total_tokens():
-    balance_1 = token_contract.functions.balanceOf(w3.toChecksumAddress(ADDRESS_1)).call()
-    balance_2 = token_contract.functions.balanceOf(w3.toChecksumAddress(ADDRESS_2)).call()
-    total_tokens = balance_1 + balance_2
-    return total_tokens
+# Получаем информацию о количестве токенов на каждом кошельке
+total_tokens = 0
+for i, wallet_address in enumerate(wallet_addresses):
+    balance = contract.functions.balanceOf(wallet_address).call()
+    total_tokens += balance
+    print(f"Wallet {i+1}: {balance} tokens")
 
-# Установить значение TOTAL_TOKENS равным общему количеству токенов на всех кошельках
-TOTAL_TOKENS = get_total_tokens()
+# Получаем текущую среднюю стоимость газа
+gas_price = w3.eth.gas_price
 
-# Функция для клейма токенов
-def claim_tokens(private_key, address):
-    # Получить аккаунт
-    account = w3.eth.account.privateKeyToAccount(private_key)
+# Устанавливаем максимальный лимит газа, который мы готовы заплатить
+gas_limit = w3.toWei(50, 'gwei')
 
-    # Получить nonce
-    nonce = w3.eth.getTransactionCount(account.address)
+# Проверяем, достаточно ли токенов на наших кошельках для клейма
+if total_tokens < contract.functions.TOTAL_SUPPLY().call():
+    print("Not enough tokens to claim")
+    exit()
 
-    # Получить значение gas price из конфигурации
-    gas_price = int(GAS_PRICE)
-
-    # Получить значение gas limit из конфигурации
-    gas_limit = int(GAS_LIMIT)
-
-    # Создать транзакцию
-    tx = {
-        'nonce': nonce,
-        'to': TOKEN_ADDRESS,
-        'value': 0,
-        'gas': gas_limit,
-        'gasPrice': gas_price,
-        'data': token_contract.functions.transferFrom(w3.toChecksumAddress(address), account.address, TOTAL_TOKENS).buildTransaction({'from': w3.toChecksumAddress(address)})['data'],
-    }
-
-    # Подписать транзакцию
-    signed_tx = w3.eth.account.signTransaction(tx, private_key=private_key)
-
-    # Отправить транзакцию
-    tx_hash = w3.eth.sendRawTransaction(signed_tx.rawTransaction)
-
-    # Вывести хеш транзакции
-    print(f"Transaction sent: {tx_hash.hex()}")
-
-# Вызвать функцию для клейма токенов на первом кошельке
-claim_tokens(PRIVATE_KEY_1, ADDRESS_1)
-
-# Вызвать функцию для клейма токен
+# Вычисляем количество токенов, которые мы можем заклеймить, учитывая максимальный лимит газа
+claimable_tokens = 0
+for i, wallet_address in enumerate(wallet_addresses):
+    balance = contract.functions.balanceOf(wallet_address).call()
+    gas_required = contract.functions.claim.estimateGas({'from': wallet_address})
+    gas_cost = gas_required * gas_price
+    if gas_cost <= gas_limit:
+        claimable_tokens += balance
+        print(f"Wallet {i+1} can claim {balance} tokens")
+    else:
+        max_claimable_tokens = int((gas_limit / gas_price) * 0.9)  # учитываем комиссию за транзакцию
